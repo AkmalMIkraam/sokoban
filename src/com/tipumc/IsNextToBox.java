7ed72709666fb9ad25c4ed3087370084264a5384
Markus <marwes91@gmail.com>
2013-10-07 16:13:33 +0200

    Added class to ask if there is a box on LEFT, RIGHT... side

---
+++ src/com/tipumc/IsNextToBox.java
@@ -0,0 +1,20 @@
+package com.tipumc;
+
+public class IsNextToBox implements SearchTest {
+
+    /**
+     *
+     * @param direction Is it a box in this direction?
+     */
+    public IsNextToBox(Move direction)
+    {
+        direction.getDirection(dir);
+    }
+
+    @Override
+    public boolean isEnd(State state, int x, int y) {
+        return state.isBox(x + dir.x, y + dir.y);
+    }
+
+    Position dir = new Position();
+}
