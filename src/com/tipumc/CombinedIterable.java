de33ba48eb807a2632ee54b43911ad8ce8e2c30e
Markus <marwes91@gmail.com>
2013-10-07 14:51:29 +0200

    Tested and fixed CombinedIterable

--- src/com/tipumc/CombinedIterable.java
+++ src/com/tipumc/CombinedIterable.java
@@ -4,16 +4,48 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class CombinedIterable<T> implements Iterable<T> {
-
-    public add(Iterable<T> iter)
+    private class CombinedIterator implements Iterator<T>
     {
-        
+        public CombinedIterator(ArrayList<Iterable<T>> iterators)
+        {
+            this.iterators = iterators;
+            this.current = iterators.get(0).iterator();
+        }
+
+        public boolean hasNext()
+        {
+            return current.hasNext() || (index+1 < iterables.size() && iterables.get(index+1).iterator().hasNext());
+        }
+
+        public T next()
+        {
+            if (current == null || !current.hasNext())
+            {
+                current = iterables.get(++index).iterator();
+            }
+            return current.next();
+        }
+
+        public void remove()
+        {
+            current.remove();
+        }
+
+        ArrayList<Iterable<T>> iterators;
+        Iterator<T> current;
+        int index;
+    }
+
+
+    public void add(Iterable<T> iter)
+    {
+        iterables.add(iter);
     }
 
     @Override
     public Iterator<T> iterator() {
-
+        return new CombinedIterator(iterables);
     }
 
-    ArrayList<T> iterables = new ArrayList<T>();
+    ArrayList<Iterable<T>> iterables = new ArrayList<Iterable<T>>();
 }
