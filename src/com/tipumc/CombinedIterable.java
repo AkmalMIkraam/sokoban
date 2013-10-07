6852caaba3225f955959f9b4d39ea5656d092a24
Markus <marwes91@gmail.com>
2013-10-07 14:15:17 +0200

    Added Iterable which can combine iterables

---
+++ src/com/tipumc/CombinedIterable.java
@@ -0,0 +1,19 @@
+package com.tipumc;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class CombinedIterable<T> implements Iterable<T> {
+
+    public add(Iterable<T> iter)
+    {
+        
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+
+    }
+
+    ArrayList<T> iterables = new ArrayList<T>();
+}
