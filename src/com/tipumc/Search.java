7bc571127dfef86ce6823fbb9788e7f3bbaf8610
Markus <marwes91@gmail.com>
2013-10-07 15:01:34 +0200

    Added findAll method

--- src/com/tipumc/Search.java
+++ src/com/tipumc/Search.java
@@ -2,6 +2,8 @@ package com.tipumc;
 
 
 
+import com.sun.jmx.remote.internal.ArrayQueue;
+
 import java.util.ArrayList;
 import java.util.Stack;
 
@@ -85,5 +87,58 @@ public final class Search {
             y -= dir.y;
         }
         return path;
+    }
+
+    public static ArrayList<Position> findAll(State state, SearchTest test, int startX, int startY)
+    {
+        /* Create stack to store nodes */
+        Stack<Position> nodes = new Stack<Position>();
+        /* Create integers that is needed */
+        int width = state.getWidth();
+        int height = state.getHeight();
+        /* Create 2D array to store visited positions */
+        Move visitedPositions[][] = new Move[width][height];
+        /* Declare an positionobject to pop to from stack */
+        Position currentPosition = new Position(startX, startY);
+        int x = currentPosition.x, y = currentPosition.y;
+
+        /* Push the start position node, for the search on the stack */
+        nodes.push(currentPosition);
+
+        ArrayList<Position> result = new ArrayList<Position>();
+        /* Search for a path to the wanted goal */
+        while (!nodes.empty()){
+            currentPosition = nodes.pop();
+            x = currentPosition.x;
+            y = currentPosition.y;
+            if (test.isEnd(state, x, y))
+            {
+                result.add(new Position(x, y));
+            }
+
+            /* Create child nodes */
+            if (visitedPositions[x][y-1] == null && (state.isFree(x, y-1) || test.isEnd(state, x, y-1))){
+                Position possibleStep = new Position(x, y-1);
+                visitedPositions[x][y-1] = Move.UP;
+                nodes.push(possibleStep);
+            }
+            if (visitedPositions[x][y+1] == null && (state.isFree(x, y+1) || test.isEnd(state, x, y+1))){
+                Position possibleStep = new Position(x, y+1);
+                visitedPositions[x][y+1] = Move.DOWN;
+                nodes.push(possibleStep);
+            }
+            if (visitedPositions[x-1][y] == null && (state.isFree(x-1, y) || test.isEnd(state, x-1, y))){
+                Position possibleStep = new Position(x-1, y);
+                visitedPositions[x-1][y] = Move.LEFT;
+                nodes.push(possibleStep);
+            }
+            if (visitedPositions[x+1][y] == null && (state.isFree(x+1, y) || test.isEnd(state, x+1, y))){
+                Position possibleStep = new Position(x+1, y);
+                visitedPositions[x+1][y] = Move.RIGHT;
+                nodes.push(possibleStep);
+            }
+        }
+
+        return result;
     }
 }
