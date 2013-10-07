d1ed8faddb07995a4b0c2852f37d8cddbba0ce43
Markus <marwes91@gmail.com>
2013-10-07 13:37:38 +0200

    Added toString and getDirection methods to Move

--- src/com/tipumc/Move.java
+++ src/com/tipumc/Move.java
@@ -4,5 +4,38 @@ public enum Move {
     LEFT,
     RIGHT,
     UP,
-    DOWN
+    DOWN;
+
+    void getDirection(Position dir)
+    {
+        dir.x = dir.y = 0;
+        switch (this)
+        {
+        case LEFT:
+            dir.x = -1;
+        case RIGHT:
+            dir.x = 1;
+        case UP:
+            dir.y = -1;
+        case DOWN:
+            dir.y = 1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        switch (this)
+        {
+            case LEFT:
+                return "L";
+            case RIGHT:
+                return "R";
+            case UP:
+                return "U";
+            case DOWN:
+                return "D";
+            default:
+                return null;
+        }
+    }
 }
